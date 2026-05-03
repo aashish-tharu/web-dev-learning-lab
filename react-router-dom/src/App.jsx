d92abdd11ebae5d5d037f9893ae15e03b656c0ba
Aashish Tharu <aashishtharugh@gmail.com>
2026-05-03 18:16:41 +0530

    feat: implemented basic routing

--- react-router-dom/src/App.jsx
+++ react-router-dom/src/App.jsx
@@ -1,23 +1,28 @@
 import './App.css'
+import { createBrowserRouter,  RouterProvider} from 'react-router-dom';
 import Home from './components/Home'
+import Dashboard from './components/Dashboard'
 import About from './components/About'
-import Profile from './components/Profile'
-import Navbar from './components/Navbar'
-import Layout from './components/Layout'
-import {Routes} from 'react-router-dom'
-import {Route} from 'react-router-dom'
 
 function App() {
+  let router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/dashboard',
+      element: <Dashboard />
+    },
+    {
+      path: '/about',
+      element: <About />
+    }
+  ])
 
   return (
     <>
-      <Routes>
-        <Route path = "/" element = {<Layout />} >
-          <Route path = "/home" element={<Home />} />
-          <Route path = "/profile/:username" element={<Profile />} />
-          <Route path = "/about" element={<About />} />
-        </Route>
-      </Routes>
+      <RouterProvider router = {router} />
     </>
   )
 }
